7d9cd78ee86ae5bda1767149ce879dab61b8b91c
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-08-24 05:00:22 +0000

    Update script
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- scripts/gerrit.sh
+++ scripts/gerrit.sh
@@ -4,4 +4,4 @@ if [ -z "${ESENV}" ]
 then
   ESENV=test
 fi
-./gerrit --gerrit-url='gerrit.onap.org' --gerrit-es-url="`cat ./secrets/ES_URL.${ESENV}.secret`" --gerrit-user="`cat ./secrets/user.secret`" --gerrit-ssh-key="`cat ./secrets/key.secret`" --gerrit-ssh-key-path='$HOME/.ssh/id_rsa' --gerrit-disable-host-key-check $*
+./gerrit --gerrit-url='gerrit.onap.org' --gerrit-es-url="`cat ./secrets/ES_URL.${ESENV}.secret`" --gerrit-user="`cat ./secrets/user.secret`" --gerrit-ssh-key="`cat ./secrets/key.secret`" --gerrit-ssh-key-path="`cat ./secrets/key-path.secret`" --gerrit-disable-host-key-check $*
