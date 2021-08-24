cd15b71acef07ddd60636eaeb98a4f7dad38d359
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-08-24 11:19:01 +0000

    Script update
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- scripts/example_run.sh
+++ scripts/example_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # clear; GERRIT_TAGS="c,d,e" ./scripts/gerrit.sh --gerrit-date-from "2021-07" --gerrit-date-to "2021-08" --gerrit-pack-size=500 --gerrit-max-reviews=1000 --gerrit-tags="a,b,c" --gerrit-project=ONAP --gerrit-debug=0
-clear; ./scripts/gerrit.sh --gerrit-pack-size=1000 --gerrit-max-reviews=1000 --gerrit-project=ONAP
+clear; ./scripts/gerrit.sh --gerrit-pack-size=1000 --gerrit-max-reviews=1000 --gerrit-project=ONAP --gerrit-date-from=2000
